56413277bf2522ecaf6dc166ebbbab1e451be430
SomaskandanEaswaran <79537233+SomaskandanEaswaran@users.noreply.github.com>
2021-04-21 11:34:57 +0530

    Add files via upload

---
+++ Reversing a string.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Enter a string and the program will reverse it and print it out.
+
+# In[1]:
+
+
+def rev(texts):
+    content = texts
+    print("The text to be reversed:" , content)
+    a = texts[::-1]
+    print("The text reversed:" , a)
+    
+rev('print')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[22]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
